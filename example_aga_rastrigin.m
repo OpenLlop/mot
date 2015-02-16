7989396fc47f21feabd7fbf7843910b77a6542fd
David de la Torre Sangrà <dts1990@gmail.com>
2015-02-16 21:05:52 +0100

    Minor improvements on AGA test script

--- example_aga_rastrigin.m
+++ example_aga_rastrigin.m
@@ -1,5 +1,5 @@
 %% Example AGA
-% Find minima of a function with a Genetic Algorithm
+% Find minima of a function with Genetic Algorithm
 % Manel Soria, David de la Torre and Arnau Miro - ETSEIAT
 
 % Clean-up
@@ -21,12 +21,16 @@ N = [3,... % Number of elites
     floor(np*0.2)]; % Number of parents
 goal = 1E-5; % Target fitness value
 
+% Define GA info data
+info = 2; % Verbosity level (1=minimal, 2=extended)
+label = 10; % Label (identification purposes)
+
 % Auxiliary function
 ranrange = @(a,b,n) a + (b-a)*rand(n,1); % n random values between a i b
 
 % Define GA functions
 funique = @(x) x; % Discard identical individuals: currently not in use
-fitfun = @(x) ras(x(1),x(2));  % Fitness function - TO BE MINIMIZED
+fitfun = @(x) ras(x(1),x(2)); % Fitness function - TO BE MINIMIZED
 mutfun = @(x,f) x + ranrange(-0.1,0.1,2); % Mutation: small random mov
 repfun = @(x,y) (x+y)/2; % Reproduction: average
 ranfun = @() ranrange(-200,200,2); % Random individual
@@ -35,28 +39,23 @@ prifun = @(x) fprintf('%f %f ',x(1),x(2)); % Print an individual
 % Randomize random seed
 rng('shuffle'); % We don't want repetability in the GA
 
-% Define GA info data
-info = 2; % Verbosity level
-label = 10; % Label
-
 % Execute Genetic Algorithm
-[bestPopGA,bestFitValGA,nite,history] = aga(info,label, ...
-                 np,...
-                 ng,N,goal,...
-                 funique,fitfun,mutfun,repfun,ranfun,prifun);
-    % pop2 is the sorted population after iterating; pop2{1} is the best solution available
-    % best = fitfun(pop2{1}) is the optimal value
-    % nite is the number of iterations performed
-bestIndGA = bestPopGA{1}; % Best Individual of GA algorithm
+[bestPopGA,bestFitGA,...
+    nite,history] = aga(info,label, ...
+                        np,ng,N,goal,...
+                        funique,fitfun,mutfun,repfun,ranfun,prifun);
+
+% Best Individual of GA algorithm
+bestIndGA = bestPopGA{1};
 
 % Now, we can easily improve the accuracy of the local extremum found
 options = optimset('TolFun',1e-8,'Display','none');
-[bestIndFMS,bestFitValFMS] = fminsearch(fitfun,bestIndGA,options);
+[bestIndFMS,bestFitFMS] = fminsearch(fitfun,bestIndGA,options);
 
 % Display results of aga and fminsearch algorithms
 fprintf('\nAlgorithm \tBest individual (x,y) \tValue\n');
-fprintf('AGA \t\t%1.6f,%1.6f \t\t%1.6E\n',bestIndGA,bestFitValGA);
-fprintf('FMS \t\t%1.6f,%1.6f \t\t%1.6E\n',bestIndFMS,bestFitValFMS);
+fprintf('AGA \t\t%1.6f,%1.6f \t\t%1.6E\n',bestIndGA,bestFitGA);
+fprintf('FMS \t\t%1.6f,%1.6f \t\t%1.6E\n',bestIndFMS,bestFitFMS);
 
 %% Plot history
 

80c575932a4459aca579676641151d2857c571c1
David de la Torre Sangrà <dts1990@gmail.com>
2015-04-14 19:49:23 +0200

    Documented and improved APS (Particle Swarm) function and its example script. Moved aps function and example script to main directory.

---
+++ aps.m
@@ -0,0 +1,157 @@
+function [ bestind, bestfit, nite, lastpop, lastfit, history ] = ...
+    aps ( opts, pop, v, c1, c2, P1, nitemax, goal, ... 
+    fitfun, prifun)
+%APS Iterates to find minimum of a function using Particle Swarm (PS)
+%
+%Programmers:   Manel Soria         (UPC/ETSEIAT)
+%               David de la Torre	(UPC/ETSEIAT)
+%Date:          14/04/2015
+%Revision:      1
+%
+%Usage:         [bestind, bestfit, nite, lastpop, lastfit, history] = ...
+%                   aps ( opts, pop, v, c1, c2, P1, nite, goal, ...
+%                   fitfun, prifun )
+%
+%Inputs:
+%   opts:       function control parameters [struct] (optional)
+%       ninfo:  verbosity level (0=none, 1=minimal, 2=extended)
+%       label:  integer number that precedes the prints in case output is
+%               to be filtered
+%       dopar:  parallel execution of fitness function [1,0]
+%       nhist:  save history (0=none, 1=fitness, 2=all{pop,fit})
+%                   0: history = []
+%                   1: history(ng) = bestfit(i)
+%                   2: history{ng,1:2} = {pop,fitness}
+%   pop:        initial population
+%   v:          initial population velocity
+%   c1:         local learning factor
+%   c2:         global learning factor
+%   P1:         maximum step size (velocity) allowed in one iteration
+%               expressed as a fraction of the domain size
+%               The domain size is estimated based on the initial
+%               distribution of the particles
+%               P1 should be about 1E-2
+%               Low values of P1 slow down the algorithm, too high values
+%               can be unstable
+%   nitemax:    maximum number of iterations allowed
+%   goal:       If function value is below goal, iterations are stopped
+%
+%   Call back functions to be provided by the user:
+%   fitfun:     fitness function
+%   prifun:     Prints individual
+%
+%Outputs:
+%   bestind:    best individual from the last generation
+%   bestfit:    fitness value of best individual from the last population
+%   nite:       number of iterations performed
+%   lastpop:    population of last generation
+%   lastfit:    fitness values of last population
+%   history:    array with saved history array
+
+% Set options
+if isfield(opts,'ninfo'), ninfo = opts.ninfo; else ninfo = 1; end;
+if isfield(opts,'label'), label = opts.label; else label = 0; end;
+if isfield(opts,'dopar'), dopar = opts.dopar; else dopar = 0; end;
+if isfield(opts,'nhist'), nhist = opts.nhist; else nhist = 1; end;
+
+% Declare history array, if required
+if nhist>0 || nargout>3, history = []; end;
+
+% Preprocessing
+np = length(pop); % Population size
+fi = zeros(np,1); % Fitness of each particle
+fib = zeros(np,1); % Personal best fitness of each particle
+popb = pop; % Personal best position of each particle
+bestfit = 0; % Best global fitness
+
+% Estimate the domain size to limit particle velocity
+xmax = pop{1}; % Select first particle as starter
+xmin = pop{1};
+for i=2:np % Get the global max/min position of the entire population
+    xmax = max(xmax,pop{i}); % Global maximum position
+    xmin = min(xmin,pop{i}); % Global minimum position
+end;
+vmax = P1 * norm(xmax-xmin); % Maximum allowed velocity
+
+% Iterate until convergence or max iterations
+for ite=1:nitemax
+
+    % Evaluate fitness function
+    if dopar % Parallel execution
+        parfor i=1:np, fi(i) = feval(fitfun,pop{i}); end;
+    else % Serial execution
+        for i=1:np, fi(i) = feval(fitfun,pop{i}); end;
+    end;
+
+    % Update personal best of each particle
+    for i=1:np
+        if fi(i)<fib(i) || ite==1
+            fib(i) = fi(i); % Best fitness
+            popb{i} = pop{i}; % Best particle position
+        end;
+    end;
+    
+    % Get global minimum fitness value and its particle
+    [minfit, mfp] = min(fi);
+
+    % If a better particle is found, update global best values
+    if minfit<bestfit || ite==1
+        bestind = pop{mfp}; % New global best particle
+        bestfit = minfit; % New global best fitness
+    end;
+    
+    % Save history
+    if nhist>1 % Save full history {population,fitness}
+        history{ite,1} = pop; %#ok
+        history{ite,2} = fi; %#ok
+    elseif nhist>0 % Save best fitness only
+        history(ite) = fi(1); %#ok
+    end;
+    
+    % Show info if required
+    if ninfo>1
+        fprintf('PS label=%d ite=%d',label,ite); % Print info
+        fprintf(' best= '); prifun(pop{mfp}); % Print best individual
+        fprintf(' vbest= '); prifun(v{mfp}); % Print best individual v
+        fprintf(' fbest=%e \n',minfit); % Print best individual fitness
+    end;
+    
+    % Check if reached target fitness or max iterations
+    if minfit<goal || ite>=nitemax % Target achieved
+        
+        % Save last iteration data
+        bestind = pop{mfp}; % Save best individual
+        bestfit = minfit; % Save fitness level of last best individual
+        nite = ite; % Save current generation index
+        lastpop = pop; % Save last population
+        lastfit = fi; % Save last fitness values
+        
+        % Stop iterating
+        break;
+        
+    end;
+    
+    % Update positions
+    for i=1:np
+        pop{i} = pop{i} + v{i}; % Add velocity for one time step
+    end
+
+    % Update velocities
+    for i=1:np
+        v{i} = v{i} ... % Previous velocity
+            + c1 * rand() * (popb{i} - pop{i}) ... % Local learning 
+            + c2 * rand() * (bestind - pop{i}); % Global learning
+    end;
+   
+    % Limit velocities
+    for i=1:np
+        nvi = norm(v{i}); % Velocity norm
+        if nvi>vmax % Ecessive velocity
+            v{i} = v{i} * vmax / nvi; % Scale velocity
+        end;
+    end;
+    
+end;
+
+end
+
---
+++ example_aps_rastrigin.m
@@ -0,0 +1,138 @@
+% Example APS
+% Find minima of a function with Particle Swarm (PS) algorithm
+% Manel Soria, David de la Torre and Arnau Miro - ETSEIAT
+
+% Clean-up
+close all;
+clear;
+
+%% APS
+
+% Our test is a R^2->R function based on Rastrigin function.
+% It is challenging because it has infinite local extrema, located at
+% integer numbers (ie, 8,-9)
+% The global minimum is at (1,1), and its value is 0
+ras = @(x,y) 20+(x-1).^2+(y-1).^2-10*(cos(2*pi*(x-1))+cos(2*pi*(y-1)));
+
+% Define PS function options (optional)
+opts.ninfo = 2; % Verbosity level (0=none, 1=minimal, 2=extended)
+opts.label = 10; % Label (identification purposes)
+opts.dopar = 0; % Parallel execution of fitness function
+opts.nhist = 2; % Save history (0=none, 1=fitness, 2=all{pop,fit})
+
+% Define PS parameters
+nitemax = 50; % Maximum number of iterations
+np = 50; % Population size
+c1 = 9; % Local learning factor
+c2 = 20; % Global learning factor
+P1 = 1E-2; % Maximum step size (velocity) allowed in one iteration
+goal = 1E-5; % Target fitness value
+
+% Auxiliary function
+ranrange = @(a,b,n) a + (b-a)*rand(n,1); % n random values between a i b
+
+% Define PS functions
+fitfun = @(x) ras(x(1),x(2)); % Fitness function - TO BE MINIMIZED
+ranfun = @() ranrange(-5,5,2); % Random individual
+prifun = @(x) fprintf('%f %f ',x(1),x(2)); % Print an individual
+
+% Randomize random seed
+rng('shuffle'); % We don't want repeatability in the PS
+
+% Generate initial population
+for i=1:np
+    pop{i} = ranfun(); %#ok
+    v{i} = 0.1 * ranfun(); %#ok
+end;
+
+% Execute Particle Swarm
+[ bestIndAPS, bestFitAPS, nite, lastPopAPS, lastFitAPS, history ] = ...
+    aps ( opts, pop, v, c1, c2, P1, nitemax, goal, ... 
+    fitfun, prifun );
+
+% Now, we can easily improve the accuracy of the local extremum found
+options = optimset('TolFun',1E-8,'Display','none');
+[bestIndFMS,bestFitFMS] = fminsearch(fitfun,bestIndAPS,options);
+
+% Display results of aps and fminsearch algorithms
+fprintf('\nAlgorithm \tBest individual (x,y) \tValue\n');
+fprintf('APS \t\t%1.6f,%1.6f \t\t%1.6E\n',bestIndAPS,bestFitAPS);
+fprintf('FMS \t\t%1.6f,%1.6f \t\t%1.6E\n',bestIndFMS,bestFitFMS);
+
+%% Fitness plot
+
+% Get fitness history
+if opts.nhist>1 && iscell(history) % Full history; get fitness values
+    fithist = zeros(length(history),1);
+    for i=1:length(history)
+        fithist(i) = history{i,2}(1);
+    end;
+else fithist = history; % Simple history
+end;
+
+% Plot data
+if ~isempty(fithist)
+
+    % Create figure
+    fh1 = figure('Position',[400,200,900,600]);
+
+    % Plot history
+    semilogy(fithist,'o-');
+
+    % Beautify plot
+    grid minor;
+    title('Particle Swarm optimization | Rastrigin function');
+    xlabel('Iteration [#]');
+    ylabel('Best fitness function value [log]');
+
+end;
+
+%% Generations plot
+
+% Only show generations when outputting full history
+if opts.nhist>1 && iscell(history)
+
+    % Create figure
+    fh2 = figure('Position',[400,200,900,600]);
+
+    % Plot rastrigin function
+    [x,y] = meshgrid(-5:0.05:5,-5:0.05:5); z = ras(x,y);
+    bh = surf(x,y,z,'LineStyle','none');
+    colorbar('Location','EastOutside');
+    view(0,90); hold on;
+
+    % Plot generations
+    ph = cell(np,1); % Handles
+    for iter=1:length(history)
+
+        % Title
+        title({'Particle Swarm optimization | Rastrigin function';...
+            sprintf('Iteration %03.0f',iter)});
+        
+        % Plot individuals
+        for i=1:np
+
+            % Plot individual
+            x = history{iter,1}{i}(1);
+            y = history{iter,1}{i}(2);
+            z = 100;
+            ph{i} = plot3(x,y,z,'ro','MarkerSize',6);
+
+        end;
+
+        % Do events
+        axis([-5 5 -5 5]);
+        drawnow;
+        
+        % Wait
+        pause(0.1);
+
+        % Delete individuals
+        if iter~=length(history) % Keep last frame
+            for i=1:np, delete(ph{i}); end;
+        end;
+
+    end;
+
+end;
+
